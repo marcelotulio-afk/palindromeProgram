03f06d30df0b23ad3cc62649077efe224943a4d1
marcelotulio-afk <71139182+marcelotulio-afk@users.noreply.github.com>
2021-01-26 14:17:58 -0300

    Create GFG.java

---
+++ GFG.java
@@ -0,0 +1,60 @@
+// A solution for the longest palindrome
+import java.util.*;
+
+class GFG 
+{
+  // Function to print a substring str[low..high]
+  static void printSubStr(String str, int low, int high)
+  {
+    for (int i = low; i <= high; ++i)
+    System.out.print(str.charAt(i));
+  }
+
+  // This function prints the
+  // longest palindrome subString
+  // It also returns the length
+  // of the longest palindrome
+  static int longestPalSubstr (String str)
+
+  {
+    // get the length of the input String
+    int n = str.length();
+
+    // All substrings of length 1
+    // are palindromes
+    int maxLength = 1, start = 0;
+
+    // Nested loop to mark start and end index
+    for (int i = 0; i < str.length(); i++)
+    {
+      for (int j = i; j < str.length(); j++)
+      {
+        int flag = 1;
+
+        // Check the palindrome
+        for (int k = 0; k < (j - i + 1)/2; k++)
+        if(str.charAt(i + k) != str.charAt(j - k))
+        flag = 0;
+
+        // Palindrome
+        if (flag != 0 && (j - i + 1) > maxLength)
+        {
+          start = i;
+          maxLength = j - i + 1;
+        }
+      }
+    }
+    System.out.print("longest string palindrome is: ");
+    printSubStr(str, start, start + maxLength - 1);
+
+    // return length of LPS
+    return maxLength;
+  }
+
+  // Driver Code
+  public static void main(String[] args)
+  {
+    String str = "abccbadef";
+    System.out.print("\nLength is: " + longestPalSubstr(str));
+  }
+}
